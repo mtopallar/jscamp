50642a02dcc54cbef293d1e2d222d9e5f50a9503
Murat Topallar <topallarm@outlook.com>
2021-11-22 23:48:14 +0300

    22.11.2021 Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,6 +1,7 @@
 import UserService from "../services/userService.js"
 import User from './../models/user.js';
 import { BaseLogger, MongoLogger } from './../crossCuttingConcerns/logging/logger.js';
+import Customer from "../models/customer.js";
 
 //Ekran simülasyonu.
 console.log("User component yüklendi")
@@ -15,11 +16,26 @@ let user2 = new User(2,"Baran","Gökçekli","Muğla")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
 
 //Playground :)
 let customer = {id:1, firstName:"Engin"}
 customer.lastName="Demiroğ" //prototyping (sonradan değer ekleme)
 //console.log(customer.lastName)
+
+
+console.log("-----------------------------------")
+
+userService.load()
+
+
+
+let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","sfsdfsdf")
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+console.log(userService.customers),
+console.log(userService.employees),
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 export default class User{
-    constructor(id, firstName, lastName, city){ 
+    constructor(id, firstName, lastName, city, age){ 
         //prototyping yapıyoruz
         //ctor yine class newlenince çalışıyor.
         // this demek User class ı demek
@@ -7,6 +7,7 @@ export default class User{
         this.firstName = firstName //this de firstName diye bişey oluştur onun da değeri parametre ile gelen firstName olsun işte bu tam da prototyping oluyor.
         this.lastName= lastName
         this.city = city
+        this.age = age
 
 
     }
